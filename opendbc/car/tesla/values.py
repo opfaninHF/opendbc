06a6354e205ec5d17acedba5cf93e8ccdf431c1c
mmoo758 <yccao94@gmail.com>
2026-04-09 11:38:52 +0800

    dev260408

--- opendbc/car/tesla/values.py
+++ opendbc/car/tesla/values.py
@@ -133,7 +133,7 @@ class CarControllerParams:
   ACCEL_MIN = -3.48  # m/s^2
   JERK_LIMIT_MAX = 4.9  # m/s^3, ACC faults at 5.0
   JERK_LIMIT_MIN = -4.9  # m/s^3, ACC faults at 5.0
-  JERK_RATE_UP = 16.0  # m/s^3 per second ramp rate (fixes gas override issue)
+  JERK_RATE_UP = 1.0  # m/s^3 per second ramp rate (fixes gas override issue)
 
 
 class TeslaSafetyFlags(IntFlag):
